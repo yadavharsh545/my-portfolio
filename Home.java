0592a5e9a87e1063a5dc804e357cf9929fa0d0e5
Harsh Kumar Yadav <yadavharsh545@gmail.com>
2024-10-07 13:36:17 +0530

    Add files via upload

---
+++ Home.java
@@ -0,0 +1,6 @@
+public class Home {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+// Burger Menu Toggle
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+});
